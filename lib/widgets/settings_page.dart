01bd88ac5ad9ed2dddc16f5fa85dc0ec2431806f
LeenArtil <lma241@usal.edu.lb>
2025-08-28 09:41:38 +0300

    settings

--- lib/widgets/settings_page.dart
+++ lib/widgets/settings_page.dart
@@ -10,136 +10,131 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF3859C5),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Settings",
-                    style: TextStyle(
-                      fontFamily: "Gilroy",
-                      fontWeight: FontWeight.w800,
-                      fontSize: 28,
-                      color: Colors.white,
-                    ),
-                  ),
-                  IconButton(
-                    icon: SvgPicture.asset(
-                      'assets/icons/xbutton.svg',
-                      color: Colors.white,
-                      height: 16,
-                      width: 16,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
+    body: SafeArea(
+  child: Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Settings",
+              style: TextStyle(
+                fontFamily: "Gilroy",
+                fontWeight: FontWeight.w800,
+                fontSize: 28,
+                color: Colors.white,
               ),
-
-              const SizedBox(height: 24),
-
-              CustomExpandableTile(
-                title: "Language",
-                options: [
-                  "English",
-                  "Turkce",
-                  "Spanish",
-                  "Nederlands",
-                  "Francais",
-                  "Italiana",
-                  "Polskie",
-                  "Svenska",
-                  "Potugues",
-                  "Gaeilge",
-                  "Pyccknn",
-                  "Norsk",
-                  "Bulgarian",
-                  "Finnish",
-                ],
+            ),
+            IconButton(
+              icon: SvgPicture.asset(
+                'assets/icons/xbutton.svg',
+                color: Colors.white,
+                height: 16,
+                width: 16,
               ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
 
-              const Divider(color: Colors.white24, thickness: 1),
-              CustomSecurityTile(),
+        const SizedBox(height: 24),
 
-              const Spacer(),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Account",
-                        style: TextStyle(
-                          fontFamily: "Gilroy",
-                          fontWeight: FontWeight.w800,
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        "123@gmail.com",
-                        style: TextStyle(
-                          fontFamily: "Gilroy",
-                          fontWeight: FontWeight.w300,
-                          fontSize: 16,
-                          color: Colors.white70,
-                        ),
-                      ),
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/home');
-                    },
-                    child: const Text(
-                      "Sign Out",
-                      style: TextStyle(
-                        fontFamily: "Gilroy",
-                        fontWeight: FontWeight.w800,
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 30),
-
-              const Text(
-                "Privacy Policy",
-                style: TextStyle(
-                  fontFamily: "Gilroy",
-                  fontWeight: FontWeight.w300,
-                  fontSize: 18,
-                  color: Colors.white,
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CustomExpandableTile(
+                  title: "Language",
+                  options: [
+                    "English", "Turkce", "Spanish",
+                    "Nederlands", "Francais", "Italiana",
+                    "Polskie", "Svenska", "Potugues",
+                    "Gaeilge", "Pyccknn", "Norsk",
+                    "Bulgarian", "Finnish",
+                  ],
                 ),
-              ),
-              const SizedBox(height: 6),
-              const Text(
-                "Terms And Conditions",
-                style: TextStyle(
-                  fontFamily: "Gilroy",
-                  fontWeight: FontWeight.w300,
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-              ),
-
-              const SizedBox(height: 20),
-            ],
+                const Divider(color: Colors.white24, thickness: 1),
+                CustomSecurityTile(),
+              ],
+            ),
           ),
         ),
-      ),
+
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Account",
+                  style: TextStyle(
+                    fontFamily: "Gilroy",
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  "123@gmail.com",
+                  style: TextStyle(
+                    fontFamily: "Gilroy",
+                    fontWeight: FontWeight.w300,
+                    fontSize: 16,
+                    color: Colors.white70,
+                  ),
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: const Text(
+                "Sign Out",
+                style: TextStyle(
+                  fontFamily: "Gilroy",
+                  fontWeight: FontWeight.w800,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 30),
+        const Text(
+          "Privacy Policy",
+          style: TextStyle(
+            fontFamily: "Gilroy",
+            fontWeight: FontWeight.w300,
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 6),
+        const Text(
+          "Terms And Conditions",
+          style: TextStyle(
+            fontFamily: "Gilroy",
+            fontWeight: FontWeight.w300,
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
+    ),
+  ),
+),
+
     );
   }
 }
